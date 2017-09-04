d33d6508b8eb5b1123fefb4893d671fedf45f005
Maxim Lebedev <mail@toby3d.ru>
2017-09-04 23:33:09 +0500

    :sparkles: Added getMe method

---
+++ get_me.go
@@ -0,0 +1,15 @@
+package telegram
+
+import json "github.com/pquerna/ffjson/ffjson"
+
+// GetMe testing your bot's auth token. Requires no parameters. Returns basic information about the bot in form of a User object.
+func (bot *Bot) GetMe() (*User, error) {
+	resp, err := bot.get("getMe", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var data User
+	err = json.Unmarshal(*resp.Result, &data)
+	return &data, err
+}
