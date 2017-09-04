dab73c9532bb150c00788aa2900c4859b659f0fe
Maxim Lebedev <mail@toby3d.ru>
2017-09-05 00:03:09 +0500

    :sparkles: Added getStickerSet method

---
+++ get_sticker_set.go
@@ -0,0 +1,21 @@
+package telegram
+
+import (
+	json "github.com/pquerna/ffjson/ffjson"
+	http "github.com/valyala/fasthttp"
+)
+
+// GetStickerSet get a sticker set. On success, a StickerSet object is returned.
+func (bot *Bot) GetStickerSet(name string) (*StickerSet, error) {
+	var args http.Args
+	args.Add("name", name) // Name of the sticker set
+
+	resp, err := bot.get("getStickerSet", &args)
+	if err != nil {
+		return nil, err
+	}
+
+	var data StickerSet
+	err = json.Unmarshal(*resp.Result, &data)
+	return &data, err
+}
