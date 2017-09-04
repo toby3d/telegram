e54cf25b55c8ec70f86448e741bfb18273da149a
Maxim Lebedev <mail@toby3d.ru>
2017-09-05 01:31:41 +0500

    :sparkles: Added leaveChat method

---
+++ leave_chat.go
@@ -0,0 +1,28 @@
+package telegram
+
+import (
+	json "github.com/pquerna/ffjson/ffjson"
+	http "github.com/valyala/fasthttp"
+)
+
+// LeaveChat leave a group, supergroup or channel. Returns True on success.
+func (bot *Bot) LeaveChat(chat interface{}) (bool, error) {
+	var args http.Args
+	switch id := chatID.(type) {
+	case int64: // Unique identifier for the target chat...
+		args.Add("chat_id", strconv.FormatInt(id, 10))
+	case string: // ...or username of the target supergroup or channel (in the format @channelusername)
+		args.Add("chat_id", id)
+	default:
+		return nil, errors.New(errorInt64OrString)
+	}
+
+	resp, err := bot.post("leaveChat", &args)
+	if err != nil {
+		return nil, err
+	}
+
+	var data bool
+	err = json.Unmarshal(*resp.Result, &data)
+	return data, err
+}
