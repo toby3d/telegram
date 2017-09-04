2c7898137f8c476a27650c1ee386002fe71719ff
Maxim Lebedev <mail@toby3d.ru>
2017-09-04 23:33:50 +0500

    :sparkles: Added getWebhookInfo method

---
+++ get_webhook_info.go
@@ -0,0 +1,15 @@
+package telegram
+
+import json "github.com/pquerna/ffjson/ffjson"
+
+// GetWebhookInfo get current webhook status. Requires no parameters. On success, returns a WebhookInfo object. If the bot is using getUpdates, will return an object with the url field empty.
+func (bot *Bot) GetWebhookInfo() (*WebhookInfo, error) {
+	resp, err := bot.get("getWebhookInfo", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var data WebhookInfo
+	err = json.Unmarshal(*resp.Result, &data)
+	return &data, err
+}
