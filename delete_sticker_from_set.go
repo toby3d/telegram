4e7131fb4cb9f91d626393804efca26bc2117b80
Maxim Lebedev <mail@toby3d.ru>
2017-09-05 00:13:34 +0500

    :sparkles: Added deleteStickerFromSet method

---
+++ delete_sticker_from_set.go
@@ -0,0 +1,21 @@
+package telegram
+
+import (
+	json "github.com/pquerna/ffjson/ffjson"
+	http "github.com/valyala/fasthttp"
+)
+
+// DeleteStickerFromSet delete a sticker from a set created by the bot. Returns True on success.
+func (bot *Bot) DeleteStickerFromSet(sticker string) (bool, error) {
+	var args http.Args
+	args.Add("sticker", sticker) // File identifier of the sticker
+
+	resp, err := bot.get("deleteStickerFromSet", &args)
+	if err != nil {
+		return nil, err
+	}
+
+	var data bool
+	err = json.Unmarshal(*resp.Result, &data)
+	return data, err
+}
