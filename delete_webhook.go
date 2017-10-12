ea5dd969d2ac8f42c8169dbfa1523e5ef67ad0d3
Maxim Lebedev <git@toby3d.ru>
2017-10-12 14:45:08 +0500

    :bug: Fixed DeleteWebhook method

--- delete_webhook.go
+++ delete_webhook.go
@@ -1,7 +1,16 @@
 package telegram
 
+import json "github.com/pquerna/ffjson/ffjson"
+
 // DeleteWebhook remove webhook integration if you decide to switch back to
 // getUpdates. Returns True on success. Requires no parameters.
-func (bot *Bot) DeleteWebhook() (*Response, error) {
-	return bot.request(nil, "deleteWebhook", nil)
+func (bot *Bot) DeleteWebhook() (bool, error) {
+	resp, err := bot.request(nil, "deleteWebhook", nil)
+	if err != nil {
+		return false, err
+	}
+
+	var data bool
+	err = json.Unmarshal(*resp.Result, &data)
+	return data, err
 }
