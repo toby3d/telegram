163bae541d96970431ed97d2bf6f25932ebf539d
Maxim Lebedev <git@toby3d.ru>
2018-01-29 14:06:40 +0500

    :sparkles: Added NewInlineMention helper

--- helpers.go
+++ helpers.go
@@ -1,8 +1,27 @@
 package telegram
 
+import (
+	"fmt"
+	"net/url"
+	"strconv"
+)
+
 func NewForceReply(selective bool) *ForceReply {
 	return &ForceReply{
 		ForceReply: true,
 		Selective:  selective,
 	}
+}
+
+func NewInlineMentionURL(id int) *url.URL {
+	link := &url.URL{
+		Scheme: "tg",
+		Path:   "user",
+	}
+
+	q := link.Query()
+	q.Add("id", strconv.Itoa(id))
+	link.RawQuery = q.Encode()
+
+	return link
 }
